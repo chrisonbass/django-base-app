fd707567a0c9abf1bf9629ba567430099c5a09ba
Chris M <mc.moss84@gmail.com>
2020-07-25 11:10:35 -0400

    Improved gateway routing for backend

--- vol/django/basesite/urls.py
+++ vol/django/basesite/urls.py
@@ -30,11 +30,11 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'api/users', UserViewSet)
+router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
